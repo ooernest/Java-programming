1f34d9727eaf7221499be2c896a9c4ae14b26dba
MAC <e.oyagbarha@outlook.com>
2023-10-31 16:58:25 +0100

    added new attribut

--- src/Ernest.java
+++ src/Ernest.java
@@ -1,11 +1,19 @@
 public class Ernest {
-    private String  name;
-            private int age ;
-            private String skinColour;
-            private boolean isOld;
-            private float accountBalance;
+    private String name;
+    private int age;
+    private String skinColour;
+    private boolean isOld;
+    private float accountBalance;
 
+    public Ernest (){
+        // Empty constructor
+    }
 
-
-
+    public Ernest (String name, int age, String skinColour, boolean isOld, float accountBalance){
+        this.accountBalance =accountBalance;
+        this.name = name;
+        this.age =age;
+        this.skinColour =skinColour;
+        this.isOld =isOld;
+    }
 }
