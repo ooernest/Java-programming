0121f2c8ca553f6d9d965144c928b99dd4b78f35
MAC <e.oyagbarha@outlook.com>
2023-10-31 16:25:08 +0100

    added more attributes

--- src/Ernest.java
+++ src/Ernest.java
@@ -2,6 +2,10 @@ public class Ernest {
     private String  name;
             private int age ;
             private String skinColour;
+            private boolean isOld;
+            private float accountBalance;
+
+
 
 
 }
