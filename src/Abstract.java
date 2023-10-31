5d26643a479127e6dec87ceccaddfbb58bf8e816
MAC <e.oyagbarha@outlook.com>
2023-11-01 00:07:17 +0100

    created new attributes

--- src/Abstract.java
+++ src/Abstract.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Abstract {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first Number");
+        int firstNumber = input.nextInt();
+
+        System.out.println("Enter secound Number");
+        int secondNumber = input.nextInt();
+
+        int sum = firstNumber * secondNumber;
+        System.out.println("the sum of the two numbers" + sum);
+
+
 
     }
 }
