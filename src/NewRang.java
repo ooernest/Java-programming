1231166ec3eb31b0756d6bf11c95ae0e6db2e44f
MAC <e.oyagbarha@outlook.com>
2023-11-03 23:28:11 +0100

    created array

---
+++ src/NewRang.java
@@ -0,0 +1,14 @@
+public class NewRang {
+    public static void main(String[] args) {
+       int sum = 0;
+//       by default the sum is 0
+//        then loop through 1-100
+       for (int i = 1; i <= 100; i++){
+//           while looping the discription shows a loop add 1 to a new next number
+           sum = sum +1;
+           System.out.println("The sum is" + sum);
+       }
+    }
+}
+
+// Question= create a program that calculate the sum of all numbers 1 - 100
---
+++ src/SumRang.java
@@ -0,0 +1,13 @@
+public class SumRang {
+    public static void main(String[] args) {
+        //        Created an array Variable and name it "sum" that stores list of integers.
+//        To add Value inside an array you need "{ add value here}"
+        int[] sum = {2, 5, 8, 1, 0, 4, 78, 34, 21};
+
+//        Tell the forloop to start from the beginning of the array, then
+//        tell it to stop at the lenghth of the array and of course, tell it to kick start the counting
+        for(int i = sum.length; i <= sum.length; i ++){
+            System.out.println("This is the array lenghth" +i);
+        }
+    }
+}
