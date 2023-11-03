61d0f42eec9d88aed3c91bdd603e59c7caeb8d82
MAC <e.oyagbarha@outlook.com>
2023-11-03 21:02:23 +0100

    create range

---
+++ src/Rang.java
@@ -0,0 +1,13 @@
+public class Rang {
+
+    public static void main(String[] args) {
+//        first create a variable
+        for (int i = 1; i<= 20; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+
+            }
+        }
+    }
+    }
+// write a java program to print all eve numbers between 1 and 20 using for loop.
